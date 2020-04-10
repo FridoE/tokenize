4b8ddd1aab8841741f0fda258831da811ace398b
paolomon <frithjof@fms-engel.de>
2020-04-10 23:29:45 +0200

    - added item divider for goalslist

--- app/src/main/java/com/example/tokenizetest/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/tokenizetest/ui/main/MainFragment.kt
@@ -1,23 +1,20 @@
 package com.example.tokenizetest.ui.main
 
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.ListView
-import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tokenizetest.R
-import com.example.tokenizetest.databinding.MainActivityBinding
-import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
@@ -41,16 +38,18 @@ class MainFragment : Fragment() {
     ): View {
         val rootView = inflater.inflate(R.layout.main_fragment, container, false)
         val listView = rootView.findViewById<RecyclerView>(R.id.listGoals)
-        val viewModelFactory = GoalsViewModelFactory(application = requireNotNull(this.activity).application)
+        val viewModelFactory =
+            GoalsViewModelFactory(application = requireNotNull(this.activity).application)
         val adapter = GoalsListAdapter()
 
-        goalViewModel = ViewModelProvider(requireActivity(), viewModelFactory).get(GoalsViewModel::class.java)
+        goalViewModel =
+            ViewModelProvider(requireActivity(), viewModelFactory).get(GoalsViewModel::class.java)
 
         goalViewModel = activity?.run {
             ViewModelProvider(this).get(GoalsViewModel::class.java)
         } ?: throw Exception("Invalid Activity")
 
-        goalViewModel.goalsList.observe(this, Observer{list -> adapter.submitList(list)})
+        goalViewModel.goalsList.observe(this, Observer { list -> adapter.submitList(list) })
 
         listView.layoutManager = LinearLayoutManager(this.context)
 
@@ -61,10 +60,33 @@ class MainFragment : Fragment() {
             val action = MainFragmentDirections.actionMainFragmentToAddgoalFragement()
             findNavController().navigate(action)
         }
+
+        context?.let {
+            ContextCompat.getDrawable(it, R.drawable.line_divider)
+        }?.also {  listView.addItemDecoration(GoalsListDividerItemDecoration(it))}
+
         return rootView
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+}
+
+class GoalsListDividerItemDecoration(var mDivider: Drawable) : RecyclerView.ItemDecoration() {
+    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        val left = parent.paddingLeft;
+        val right = parent.width - parent.paddingRight;
+
+        for (i in 0 until parent.childCount) {
+            val child = parent.getChildAt(i);
+            val params = child.layoutParams as RecyclerView.LayoutParams
+
+            val top = child.bottom + params.bottomMargin;
+            val bottom = top + mDivider.intrinsicHeight;
+
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+        }
     }
 }
